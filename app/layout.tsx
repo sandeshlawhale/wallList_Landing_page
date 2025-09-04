9481adf844ba8de5c97fb2f82f150b388c881e91
sandeshlawhale <sandeshlawhale@gmail.com>
2025-09-04 08:59:00 +0530

    made few changes according to version

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'wallList - List and Conquer',
+  description: 'WallList is a minimalist task management app designed to help you organize your tasks efficiently and effectively to your wallpaper.',
 }
 
 export default function RootLayout({
